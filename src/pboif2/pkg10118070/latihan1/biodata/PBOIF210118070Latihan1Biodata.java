9a04435fd0c588648c5172a58940b87e465289a8
Ailyasha <55798997+Ailyasha@users.noreply.github.com>
2019-09-25 21:24:20 +0700

    Add files via upload

---
+++ src/pboif2/pkg10118070/latihan1/biodata/PBOIF210118070Latihan1Biodata.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10118070.latihan1.biodata;
+
+/**
+ *
+ * @author
+ * NAMA     : Aufa Ilyasha
+ * Kelas    : IF2
+ * NIM      : 10118070
+ *
+ */
+public class PBOIF210118070Latihan1Biodata {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        System.out.println("BIODATA ROCKSTAR" + "\n" + "----------------");
+        System.out.println("NAMA\t\t: Aufa Ilyasha");
+        System.out.println("ALAMAT\t\t: Tubagus Ismail Dalam no.17D");
+        System.out.println("TEMPAT LAHIR\t: Batam");
+        System.out.println("TANGGAL LAHIR\t: 13 Oktober 1999");
+        System.out.println("--------------");
+        System.out.println("HOBI\t\t: Futsal");
+        System.out.println("MAKES\t\t: Nasi");
+        System.out.println("MIKES\t\t: Air");
+    }
+    
+}
